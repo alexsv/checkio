7c279f9144d105e757cd91b6307cf145ff8562dc
Oleksandr Vasylenko <ovasylenko@rebbix.com>
2013-12-19 18:17:13 +0200

    added solvers for triangules and 6-sides

--- sci_triangules.py
+++ sci_triangules.py
@@ -30,6 +30,34 @@ def checkio(inset):
         else:
             return get_row(n+1)
 
+    def solve_3(inset):
+        a, b, c = sorted(inset)
+        ra, rb, rc = map(get_row, [a, b, c])
+        if ra == rb or rb == rc and ra != rc:
+            if ra == rb
+                width, height = b - a, rc - rb
+            elif b == c:
+                width, height = c - a, rb - a
+            if width == height and width > 0:
+                return 3
+        return 0
+
+    def solve_4(inset):
+        return 0
+
+    def solve_6(inset):
+        a, b, c, d, e, f = sorted(inset)
+        ab, cd, ef = b - a, f - c, f - e
+        ra, rb, rc, rd, re, rf = map(get_row, [a, b, c, d, e, f])
+
+        if ra == rb and rc == rd and re == rf
+           and ra < rc < rf and ab > 0
+           and ef == ab and cd == 2 * ab
+           and c - a == ra:
+            return 6
+        else:
+            return 0
+
     return map(get_row, inset)
 
 if __name__ == "__main__":
