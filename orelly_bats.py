e39aebd3e828321aa0a9bc4fea913a686e652e32
Oleksandr Vasylenko <ovasylenko@rebbix.com>
2013-12-19 13:57:01 +0200

    added todo how to solve the task

---
+++ orelly_bats.py
@@ -0,0 +1,17 @@
+def checkio(bunker):
+    # parse bat coordinates
+    # parse wall coordinates
+    # for each other bat get connecting segment
+    # add it to graph, if it doesn't intersect with any wall (one bat sees another)
+    # find minimal distance betweem topleft bat and alpha bat
+    return 0
+
+
+if __name__ == "__main__":
+    checkio([
+            "B---B-",
+            "-WWW-B",
+            "-WA--B",
+            "-W-B--",
+            "-WWW-B",
+            "B-BWB-"])
